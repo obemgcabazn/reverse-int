0a16a97c2e6533a30f1e539bd3994589dab58c77
Electropompa <admin@zilmet.ru>
2022-02-24 16:15:14 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let input = n;
+  let number = Math.abs(input);
+  let string = number.toString();
+  let output = 0;
+  for (let i = 1; i <= string.length; i++) {
+    output +=  (Math.trunc(((number % (10 ** i)) * 10) / (10 ** i))) * (10 ** (string.length - i));
+  }
+  return output;
 }
